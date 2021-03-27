d05523f113b9d5a2d19d357d8f7cf2cb6a53d25d
Cassandra de la Cruz-Munoz <cassandra.delacruzmunoz@gmail.com>
2021-03-27 13:57:58 -0400

    add support for variable length quantities in midi.asm

--- midi.asm
+++ midi.asm
@@ -6,19 +6,106 @@ extern segmentNumOobErr:BYTE
 extern measureNumOobErr:BYTE
 extern velocityOorErr:BYTE
 extern pitchOorErr:BYTE
+extern errorMsg:BYTE
+extern errorMsgLen:DWORD
+timeVLQ DWORD 0
+timeVLQLen byte 0
+
 .code
 Error proto
 
+ConsoleWriteHex proto,
+	num:DWORD
+
+toVLQ proc uses edx,
+	outVal : DWORD,
+	value : DWORD
+	mov edx, value
+	cmp edx, 1FFFFFh
+	ja fourBytes
+	mov edx, value
+	cmp edx, 3FFFh
+	ja threeBytes
+	mov edx, value
+	cmp edx, 7Fh
+	ja twoBytes
+	mov edx, value
+	xor eax, eax
+	mov al, dl
+	shl eax, 24
+	mov outVal, eax
+	mov eax, 1
+	ret
+twoBytes :
+	xor eax, eax
+	mov edx, value
+	shl edx, 1
+	or edx, 8000h
+	mov al, dh
+	shl eax, 8
+	mov edx, value
+	and edx, 7fh
+	mov al, dl
+	shl eax, 16
+	mov outVal, eax
+	mov eax, 2
+	ret
+threeBytes :
+	xor eax, eax
+	mov edx, value
+	shr edx, 6
+	or edx, 8000h
+	mov al, dh
+	shl eax, 8
+	mov edx, value
+	shl edx, 1
+	or edx, 8000h
+	mov al, dh
+	shl eax, 8
+	mov edx, value
+	and edx, 7fh
+	mov al, dl
+	shl eax, 8
+	mov outVal, eax
+	mov eax, 3
+	ret
+fourBytes :
+	xor eax, eax
+	mov edx, value
+	shr edx, 13
+	or edx, 8000h
+	mov al, dh
+	shl eax, 8
+	mov edx, value
+	shr edx, 6
+	or edx, 8000h
+	mov al, dh
+	shl eax, 8
+	mov edx, value
+	shl edx, 1
+	or edx, 8000h
+	mov al, dh
+	shl eax, 8
+	mov edx, value
+	and edx, 7fh
+	mov al, dl
+	mov outVal, eax
+	mov eax, 4
+	ret
+toVLQ endp
+
+
 ; ------------------------------------------------------------------------------ -
-noteEvent proc uses ebx edx,
-	time:byte, ; the delta time of the event
+noteEvent proc uses eax ebx edx,
+	time:dword, ; the delta time of the event
 	event:byte, ; the type of note event
 	pitch:byte, ; the pitch of the note
 	velocity:byte; the loudness of the note
 ; stores a note event in the dword specified by EDI
 ; Returns: nothing
 ; ------------------------------------------------------------------------------ -
-	mov bh, time
+	invoke toVLQ, timeVLQ, time
+	mov timeVLQLen, al
 	mov bl, velocity
 	cmp bl, 80h; check that the velocity is valid
 	jb continue0; jump if velocity is valid
@@ -34,7 +121,11 @@ continue0:
 	call Error; call pitch error if invalid
 continue1:
 	mov dh, event
-	mov[edi], bh; delta time
+	mov eax, [timeVLQ]
+	mov [edi], eax
+	xor eax, eax
+	mov al, timeVLQLen
+	add edi, eax
 	mov[edi + 1], dh; note event
 	mov[edi + 2], dl; pitch in dl register
 	mov[edi + 3], BYTE PTR 40h; velocity of 64 (medium)
